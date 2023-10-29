d070008567fb5d64a1407a03576b6512564824a2
romxero <romxero@Rdubs-Mac-Studio.local>
2023-10-29 02:51:11 -0600

    add stuff for command line parsing state machine

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,60 @@ USE: main program file for shuck-n-jive
 static VERS_STRING: &str = r#"0.01"#;
 static SOFTWARE_NAME: &str = r#"shuck-n-jive"#;
 static MAX_HTML_FILENAME_STRING_SIZE: usize = 255; //type usize for declaring size of a string
+const MAX_FIRST_COMMAND_STRING_SIZE: usize = 16; //type usize for declaring size of a string
+
+static APPLICATION_HELP_STRING_BLOCK: &str = r#"
+Shuck-N-Jive
+Author: RC
+shuck-n-jive creates a static web site for causing trouble.
+
+ Find more information at: https://github.com/UmbaCode/shuck-n-jive
+
+Commands:
+  status        Check the status of the site stored in the database.
+  init          Initialize a database store for static site.
+  generate      Run a particular image on the cluster
+  add           Add an: author, article, etc.
+  delete        Delete an: author, article, etc.
+  modify        Modify an: author, article, etc.
+  help          This page.
+
+  Usage:
+    shuck-n-jive [flags] [options]
+"#;
+
+enum CommandLineFirstState {
+    //These establish the First State From the first command given
+    FirstStateExit,     //Usually this will mean Break out of the loop
+    FirstStateStatus,   //Show the status
+    FirstStateInit,     //Initialize the DB
+    FirstStateGenerate, //Generate the site
+    FirstStateAdd,      //Add
+    FirstStateDelete,   //Delete
+    FirstStateModify,   //Modify
+    FirstStateHelp,     //Help
+}
+
+enum CommandLineSecondState {
+    //These establish the Second State From the first command given
+    SecondStateMoot,
+    SecondStateAuthor,
+    SecondStateDatabase,
+    SecondStateArticle,
+    SecondStateHelp,
+    SecondStateYamlDocument,
+}
+
+enum CommandLineThirdState {
+    //These establish the Third State From the first command given
+    ThirdStateName,
+    ThirdStateId,
+    ThirdStateDatabase,
+    ThirdStateArticle,
+    ThirdStateHelp,
+    ThirdStateYamlDocument,
+    ThirdStateMarkdownDocument,
+}
 
 // Main Libraries
 //use dict::{ Dict };
@@ -114,16 +168,63 @@ struct mainPageContentBodyStruct {
 
 enum processStackJobState {
     // these are the states for the process queue for the main program
-    processExit,           //first state should be exit
+    PROCESS_EXIT,          //first state should be exit
     PROCESS_ERROR,         //show the error screen.. which is the bulk info
     PROCESS_HELP,          // show the help screen.. which is the bulk info pretty much
     PROCESS_VERSION,       //show the version of the software
     PROCESS_AUTHOR_LOOKUP, //show the author lookup from the database
 }
 
+fn first_command_state(passed_string: &str) -> CommandLineFirstState {
+    const MAX_FIRST_COMMAND_STRING_SIZE: usize = 16; //type usize for declaring size of a string
+
+    use voca_rs::*;
+
+    //let lower_command_state = ;
+
+    let mut internal_string_buffer = String::with_capacity(MAX_FIRST_COMMAND_STRING_SIZE);
+
+    internal_string_buffer.insert_str(0, &case::lower_case(passed_string));
+
+    let mut internel_state_buffer;
+
+    match internal_string_buffer.as_str() {
+        "status" => {
+            internel_state_buffer = CommandLineFirstState::FirstStateStatus;
+        }
+        "init" => {
+            internel_state_buffer = CommandLineFirstState::FirstStateInit;
+        }
+        "generate" => {
+            internel_state_buffer = CommandLineFirstState::FirstStateGenerate;
+        }
+        "add" => {
+            internel_state_buffer = CommandLineFirstState::FirstStateAdd;
+        }
+        "delete" => {
+            internel_state_buffer = CommandLineFirstState::FirstStateDelete;
+        }
+        "modify" => {
+            internel_state_buffer = CommandLineFirstState::FirstStateModify;
+        }
+        "help" => {
+            internel_state_buffer = CommandLineFirstState::FirstStateHelp;
+        }
+        _ => {
+            internel_state_buffer = CommandLineFirstState::FirstStateHelp;
+        }
+    };
+
+    return internel_state_buffer;
+}
+
 fn main() {
     //use standard collections
     use std::collections::*;
+    use std::env;
+
+    let args: Vec<String> = env::args().collect();
+    dbg!(args);
 
     let mut mainProcessStack: Vec<processStackJobState> = Vec::new();
 
